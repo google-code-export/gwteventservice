cd4398dfd78c853f3592758cb11b3e5df64e03f1
sven.strohschein <sven.strohschein@7a2b6950-acf1-11dd-a887-7ffe1a420f8d>
2009-09-11 22:35:23 +0000

    Synchronization of the listen method optimized with double-check

--- module/EventService/src/de/novanic/eventservice/service/registry/DefaultEventRegistry.java
+++ module/EventService/src/de/novanic/eventservice/service/registry/DefaultEventRegistry.java
@@ -223,14 +223,17 @@ public class DefaultEventRegistry implements EventRegistry
 				if(theMinWaitingTime > 0) {
 					Thread.sleep(theMinWaitingTime);
 				}
-				synchronized(theUserInfo) {
-					theUserInfo.schedule(myConfiguration.getTimeoutTime());
-					if(theUserInfo.isEventsEmpty()) {
-						theUserInfo.wait(myConfiguration.getMaxWaitingTime());
-					}
-					return theUserInfo.retrieveEvents();
-				}
-			} catch(InterruptedException e) {
+                if(theUserInfo.isEventsEmpty()) {
+                    //monitor for event notification and double checked
+                    synchronized(theUserInfo) {
+                        theUserInfo.schedule(myConfiguration.getTimeoutTime());
+                        if(theUserInfo.isEventsEmpty()) {
+                            theUserInfo.wait(myConfiguration.getMaxWaitingTime());
+                        }
+                    }
+                }
+                return theUserInfo.retrieveEvents();
+            } catch(InterruptedException e) {
 				LOG.error("Listen was interrupted (client id \"" + aUserId + "\")!", e);
 			}
 		}
