af38c2deca334b5c633f9f246a0ce1aab723004c
sven.strohschein <sven.strohschein@7a2b6950-acf1-11dd-a887-7ffe1a420f8d>
2009-10-29 22:12:09 +0000

    Handling of the UnlistenEventFilter at the server side optimized ; JavaDoc enhanced

--- trunk/module/EventService/src/de/novanic/eventservice/service/registry/domain/ListenDomainAccessor.java
+++ trunk/module/EventService/src/de/novanic/eventservice/service/registry/domain/ListenDomainAccessor.java
@@ -1,3 +1,22 @@
+/*
+ * GWTEventService
+ * Copyright (c) 2009, GWTEventService Committers
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package de.novanic.eventservice.service.registry.domain;
 
 import de.novanic.eventservice.client.event.domain.Domain;
@@ -5,6 +24,9 @@ import de.novanic.eventservice.client.event.domain.Domain;
 import java.util.Set;
 
 /**
+ * The ListenDomainAccessor provides methods to get all active domains where users are registered to
+ * listen for events. That information can be get for all users or for a specific user.
+ *
  * @author sstrohschein
  *         <br>Date: 29.10.2009
  *         <br>Time: 22:00:24
