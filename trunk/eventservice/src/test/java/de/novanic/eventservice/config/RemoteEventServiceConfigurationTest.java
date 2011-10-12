3c7fca5f4912ffde3e984b899dbef7bb824bf076
sven.strohschein <sven.strohschein@7a2b6950-acf1-11dd-a887-7ffe1a420f8d>
2011-10-12 17:02:16 +0000

    Copyright and project notes updated

--- trunk/eventservice/src/test/java/de/novanic/eventservice/config/RemoteEventServiceConfigurationTest.java
+++ trunk/eventservice/src/test/java/de/novanic/eventservice/config/RemoteEventServiceConfigurationTest.java
@@ -1,11 +1,13 @@
 /*
  * GWTEventService
- * Copyright (c) 2008, GWTEventService Committers
+ * Copyright (c) 2011 and beyond, strawbill UG (haftungsbeschränkt)
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
+ * Other licensing for GWTEventService may also be possible on request.
+ * Please view the license.txt of the project for more information.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
